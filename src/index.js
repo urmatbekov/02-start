ee781040c410d1b722b35d49b9aa03612f684417
Nurkadyr <nurkadyrurmatbekov@gmail.com>
2021-03-29 19:15:35 +0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom"
+import "./style.css"
+
+// const name = "Nurkadyr"
+// const age = 19
+
+
+// const app = (
+//     <h1 className="title" >
+//         My name is {name},I am {age}
+//     </h1>
+// )
+
+
+// const Text = () => {
+//     return (
+//         <p>
+//             Lorem ipsum dolor sit amet consectetur
+//             adipisicing elit. Pariatur nostrum officiis
+//             laudantium deleniti illum repellat dolore nemo
+//             in blanditiis eius. Earum, pariatur! Blanditiis incidunt
+//             tempora repellendus totam magnam neque quasi.
+//         </p>
+//     )
+// }
+
+const Header = () => {
+    return (
+        <div id="myDIV" className="header">
+            <h2>My To Do List</h2>
+            <input type="text" id="myInput" placeholder="Title..." />
+            <span className="addBtn">Add</span>
+        </div>
+    )
+}
+const Body = () => {
+    return (
+        <ul id="myUL">
+            <li>Hit the gym</li>
+            <li className="checked">Pay bills</li>
+            <li>Meet George</li>
+            <li>Buy eggs</li>
+            <li>Read a book</li>
+            <li>Organize office</li>
+        </ul>
+    )
+}
+
+const App = () => {
+    return (
+        <div>
+            <Header />
+            <Body />
+        </div>
+    )
+}
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <App />,
+    document.querySelector("#root")
+)
